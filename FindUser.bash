46afd9354ab775becd19b0a864f1effd09ef3c5c
MaldicionChina <alexisrodgtz@gmail.com>
2013-11-08 01:12:38 -0500

    Update FindUser.bash

--- FindUser.bash
+++ FindUser.bash
@@ -4,7 +4,10 @@ echo "Ingrese su Usuario: "
 read user  # se lee el usuario ingresado por consola
 echo "$user" > user.regex # se agrega el usuario a un archivo externo para su posterior lectura
 cat /etc/passwd > userPath.txt # se almacena la lista de usuarios con acceso al sistema, para luego determinar cuáles son realmente usuarios
-pathUser=$(./FindPath.awk userPath.txt) # se ejecuta el script de awk que retornar al ruta del home, en caso de que exista
+pathUser=$(./FindPath.awk userPath.txt) # se ejecuta el script de awk que retornar al ruta del 
+
+if [ "$pathUser" != "Not_Found" ]
+then
 totalDirectory=$(ls -l $pathUser | grep ^d -c) 
 totalHideDirectory=$(($(ls -la $pathUser | grep ^d -c)-$totalDirectory-2)) #al total de directorios se le resta los no ocultos y los 2 comodines punto y punto punto 
 totalShowFiles=$(ls -l $pathUser | grep ^- -c)
@@ -14,5 +17,9 @@ echo "-------N° Carpetas: " $totalDirectory
 echo "-------N° Carpetas ocultas: " $totalHideDirectory
 echo "-------N° Archivos: " $totalShowFiles
 echo "-------N° Archivos ocultos: " $totalHideFiles
+else
 
+echo " No se encontro el usuario, verifique que esté escrito correctamente"
+
+fi
 rm -r user*.* # se borran los archivos creados
